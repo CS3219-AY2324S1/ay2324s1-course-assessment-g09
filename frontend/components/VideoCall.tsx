3c104ebfc5ef035b548408b6566d4882cbdb1af3
Wilson Ng <wilsonngja@gmail.com>
2023-10-01 15:10:43 +0800

    Integrate collab service in collab tab

--- frontend/components/VideoCall.tsx
+++ frontend/components/VideoCall.tsx
@@ -15,34 +15,6 @@ function VideoCall() {
         console.log(id);
       });
     });
-    // navigator.mediaDevices
-    //   .getUserMedia({ video: true, audio: true })
-    //   .then((stream) => {
-    //     // Display the user's video stream in the webcam element
-    //     webcamRef.current.srcObject = stream;
-
-    //     // Create a PeerJS connection
-    //     const myPeer = new Peer();
-
-    //     // Handle incoming calls
-    //     myPeer.on("call", (call) => {
-    //       call.answer(stream); // Answer the call with the user's stream
-    //       call.on("stream", (remoteStream) => {
-    //         // Display the remote user's video stream
-    //         // You can create a video element for this
-    //       });
-    //     });
-    //     myPeer.on("stream", (remoteStream) => {
-    //       // Assuming you have an Audio element in your component
-    //     });
-    //     // Get your PeerJS ID and log it
-    //     myPeer.on("open", (id) => {
-    //       console.log("My peer ID is: " + id);
-    //     });
-    //   })
-    //   .catch((error) => {
-    //     console.error("Error accessing camera and microphone:", error);
-    //   });
   }, []);
 
   return (
