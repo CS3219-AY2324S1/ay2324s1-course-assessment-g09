618f46c5334d808cd68287d5508ee621816bf416
weekiat-douze <lumweekiat98@gmail.com>
2023-10-09 13:35:24 +0800

    Remove dependency next-auth from frontend

--- frontend/pages/_app.tsx
+++ frontend/pages/_app.tsx
@@ -1,6 +1,5 @@
 // pages/_app.js
 import { ChakraProvider } from "@chakra-ui/react";
-import { SessionProvider } from "next-auth/react";
 import IndexPage from ".";
 import Collaboration from "./collaboration";
 import "./index.css";
@@ -9,9 +8,7 @@ import "monaco-editor/esm/vs/base/browser/ui/actionbar/actionbar.css"; // Import
 function MyApp({ Component, pageProps: { session, ...pageProps } }) {
   return (
     <ChakraProvider>
-      <SessionProvider session={session}>
-        <Component {...pageProps} />
-      </SessionProvider>
+      <Component {...pageProps} />
       {/* <IndexPage /> */}
       {/* <Collaboration /> */}
     </ChakraProvider>
