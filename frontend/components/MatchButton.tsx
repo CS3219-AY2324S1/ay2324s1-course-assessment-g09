75c8579b43b7caa31880fd77441283e1d28cae9d
Yuxin <yuxinwu1234@gmail.com>
2023-10-10 11:19:30 +0800

    Added difficulty selection

--- frontend/components/MatchButton.tsx
+++ frontend/components/MatchButton.tsx
@@ -1,19 +1,59 @@
-import { Button } from "@chakra-ui/react";
+import {
+	Box,
+	Button,
+	Menu,
+	MenuButton,
+	MenuItem,
+	MenuList,
+} from "@chakra-ui/react";
 import { useEffect, useState } from "react";
 import { io } from "socket.io-client";
+import { ChevronDownIcon } from "@chakra-ui/icons";
 
 export default function MatchButton({ sendMatchedSocket }) {
-  const [socket, setSocket] = useState(null);
-  useEffect(() => {
-    const socket = io("http://localhost:6927");
-    setSocket(socket);
-    socket.on("matched", (event) => {
-      console.log(event);
-      sendMatchedSocket(event);
-    });
-  }, []);
-  const handleMatch = () => {
-    socket?.emit("match", "easy");
-  };
-  return <Button onClick={handleMatch}> Match</Button>;
+	const [socket, setSocket] = useState(null);
+	const [difficulty, setDifficulty] = useState("Easy");
+	useEffect(() => {
+		const socket = io("http://localhost:6927");
+		setSocket(socket);
+		socket.on("matched", (event) => {
+			console.log(event);
+			sendMatchedSocket(event);
+		});
+	}, []);
+	const handleDifficultyChange = (e) => {
+		setDifficulty(e.target.value);
+		console.log(difficulty);
+	};
+	const handleMatch = () => {
+		socket?.emit("match", difficulty);
+		console.log("matching begins");
+	};
+	return (
+		<Box>
+			<Menu>
+				<MenuButton as={Button} rightIcon={<ChevronDownIcon />}>
+					{difficulty == "Easy"
+						? "Easy"
+						: difficulty == "Medium"
+						? "Medium"
+						: difficulty == "Hard"
+						? "Hard"
+						: "Difficulty"}
+				</MenuButton>
+				<MenuList>
+					<MenuItem onClick={handleDifficultyChange} value="Easy">
+						Easy
+					</MenuItem>
+					<MenuItem onClick={handleDifficultyChange} value="Medium">
+						Medium
+					</MenuItem>
+					<MenuItem onClick={handleDifficultyChange} value="Hard">
+						Hard
+					</MenuItem>
+				</MenuList>
+			</Menu>
+			<Button onClick={handleMatch}> Match</Button>
+		</Box>
+	);
 }
