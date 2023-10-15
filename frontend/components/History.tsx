07c27329197ee47aa153e942024fd9716b1c2cce
Yuxin <yuxinwu1234@gmail.com>
2023-10-15 16:35:21 +0800

    Items listed are displayed correctly now.

--- frontend/components/History.tsx
+++ frontend/components/History.tsx
@@ -18,10 +18,23 @@ import {
 import Editor from "@monaco-editor/react";
 import { editor } from "monaco-editor";
 
+type history = {
+  _id: string;
+  questionName: string;
+  question: string;
+  code: string;
+  language: string;
+  theme: string;
+  createdAt: string;
+  updatedAt: string;
+  user1: string;
+  user2: string;
+};
 export default function History() {
   const [user, setUser] = useState("Dennis");
   const [history, setHistory] = useState(null);
   const { isOpen, onOpen, onClose } = useDisclosure();
+  const [selectedItem, setSelectedItem] = useState(null);
 
   useEffect(() => {
     const fetchHistory = async () => {
@@ -29,8 +42,25 @@ export default function History() {
         const res = await axios.get(
           `http://localhost:6969/history/get/${user}`
         );
-        console.log(res.data);
-        setHistory(res.data);
+
+        // console.log(
+        //   res.data
+        //     .slice()
+        //     .sort(
+        //       (a, b) =>
+        //         new Date(b.createdAt).getTime() -
+        //         new Date(a.createdAt).getTime()
+        //     )
+        // );
+        setHistory(
+          res.data
+            .slice()
+            .sort(
+              (a, b) =>
+                new Date(b.createdAt).getTime() -
+                new Date(a.createdAt).getTime()
+            )
+        );
       } catch (error) {
         console.log("ERROR: ", error);
       }
@@ -40,48 +70,57 @@ export default function History() {
   return (
     <Box>
       {history &&
-        history.map((record) => (
-          <Box key={record._id}>
-            <Button onClick={onOpen}>
+        history.map((record, index) => (
+          <Box key={index}>
+            <Button
+              onClick={() => {
+                setSelectedItem(record);
+                onOpen();
+              }}
+            >
+              <Text>{index}</Text>
               <Text>{record.questionName}</Text>
               <Text>
                 Date: {new Date(record.createdAt).toLocaleDateString()}, Time:
                 {new Date(record.createdAt).toLocaleTimeString()}
               </Text>
             </Button>
-            <Modal isOpen={isOpen} onClose={onClose} size="6xl">
-              <ModalOverlay />
-              <ModalContent maxW="80%" minH="80%">
-                <ModalHeader>{record.questionName}</ModalHeader>
-                <ModalCloseButton />
-                <ModalBody>
-                  <Grid templateColumns="repeat(2, 1fr)" gap={6} height="75vh">
-                    <GridItem colSpan={1}>
-                      <Text>{record.question}</Text>
-                      <Text>{record.code}</Text>
-                    </GridItem>
-                    <GridItem colSpan={1}>
-                      <Editor
-                        defaultValue={record.code}
-                        defaultLanguage={record.language}
-                        theme={record.theme}
-                        options={{
-                          readOnly: true,
-                        }}
-                      />
-                    </GridItem>
-                  </Grid>
-                </ModalBody>
-
-                <ModalFooter>
-                  <Button colorScheme="blue" mr={3} onClick={onClose}>
-                    Close
-                  </Button>
-                </ModalFooter>
-              </ModalContent>
-            </Modal>
           </Box>
         ))}
+      <Modal isOpen={isOpen} onClose={onClose} size="6xl">
+        <ModalOverlay />
+        <ModalContent maxW="80%" minH="80%">
+          {selectedItem && (
+            <Box>
+              <ModalHeader>{selectedItem.questionName}</ModalHeader>
+              <ModalCloseButton />
+              <ModalBody>
+                <Grid templateColumns="repeat(2, 1fr)" gap={6} height="75vh">
+                  <GridItem colSpan={1}>
+                    <Text>{selectedItem.question}</Text>
+                  </GridItem>
+                  <GridItem colSpan={1}>
+                    <Editor
+                      defaultValue={selectedItem.code}
+                      defaultLanguage={selectedItem.language}
+                      theme={selectedItem.theme}
+                      options={{
+                        readOnly: true,
+                      }}
+                    />
+                  </GridItem>
+                </Grid>
+              </ModalBody>
+
+              <ModalFooter>
+                <Button colorScheme="blue" mr={3} onClick={onClose}>
+                  Close
+                </Button>
+              </ModalFooter>
+            </Box>
+          )}
+        </ModalContent>
+      </Modal>
     </Box>
   );
 }
