77269d180e98d4a3fda1793830e919c7341821f1
Wilson Ng <wilsonngja@gmail.com>
2023-10-21 14:53:54 +0800

    Fix question service table bug

--- frontend/components/Questions.tsx
+++ frontend/components/Questions.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Question from "./Question";
 import questionStr from "../stores/questionsStore";
-import { Box } from "@chakra-ui/react";
+import { Box, Flex } from "@chakra-ui/react";
 import QuestionsHeader from "./QuestionsHeader";
 
 const Questions = ({
@@ -12,7 +12,7 @@ const Questions = ({
   colorMode,
 }) => {
   return (
-    <Box width="100%" marginX={12}>
+    <Box width="100%" height="100%">
       <QuestionsHeader />
       {/* <div style={{ maxHeight: "calc(100vh - 100px)", overflowY: "auto" }}> */}
       <Question
