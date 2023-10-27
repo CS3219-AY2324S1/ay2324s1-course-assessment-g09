6b7c954227e5147852cc01c33f8e3b8922fbd777
Wilson Ng <wilsonngja@gmail.com>
2023-10-27 10:59:54 +0800

    Fix questions CRUD operation

--- frontend/components/QuestionsInputField.tsx
+++ frontend/components/QuestionsInputField.tsx
@@ -35,26 +35,13 @@ const QuestionInputField = ({
   const [selectedComplexity, setSelectedComplexity] = useState("");
 
   const fetchQuestions = async () => {
-    const res = await axios.get(`${IP_ADDRESS}:3001/questions/getall`);
+    const res = await axios.get(`${IP_ADDRESS}:3001/questions`);
     setQuestions(res.data.qns);
   };
 
   const handleSubmit = async () => {
     try {
-      const res0 = await axios.get(
-        `${IP_ADDRESS}:3001/questions/get/${inputValues.qn_num}`
-      );
-
-      setError(true);
-      setInputValues({
-        ...inputValues,
-        qn_num: "",
-      });
-    } catch (error) {
-      const res = await axios.post(
-        `${IP_ADDRESS}:3001/questions/create`,
-        inputValues
-      );
+      const res = await axios.post(`${IP_ADDRESS}:3001/questions`, inputValues);
 
       console.log(res);
       setInputValues({
@@ -67,16 +54,48 @@ const QuestionInputField = ({
 
       setError(false);
       fetchQuestions();
+    } catch (error) {
+      setError(true);
+      setInputValues({
+        ...inputValues,
+        qn_num: "",
+      });
     }
+
+    // try {
+    //   const res0 = await axios.get(`${IP_ADDRESS}:3001/questions/`, {
+    //     params: {
+    //       qn_num: inputValues.qn_num,
+    //     },
+    //   });
+    //   console.log(inputValues.qn_num, res0);
+
+    //   setError(true);
+    //   setInputValues({
+    //     ...inputValues,
+    //     qn_num: "",
+    //   });
+    // } catch (error) {
+    //   const res = await axios.post(`${IP_ADDRESS}:3001/questions`, inputValues);
+
+    //   console.log(res);
+    //   setInputValues({
+    //     qn_num: "",
+    //     title: "",
+    //     description: "",
+    //     category: "",
+    //     complexity: "",
+    //   });
+
+    //   setError(false);
+    //   fetchQuestions();
+    // }
   };
 
   const handleUpdate = async () => {
     setError(false);
     const { qn_num } = inputValues;
-    await axios.post(
-      `${IP_ADDRESS}:3001/questions/update/${qn_num}`,
-      inputValues
-    );
+    await axios.put(`${IP_ADDRESS}:3001/questions/${qn_num}`, inputValues);
 
     setInputValues({
       edit_id: "",
