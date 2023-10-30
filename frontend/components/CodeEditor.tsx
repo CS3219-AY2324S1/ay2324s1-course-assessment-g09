70be7d0c6ebb499c272e4f90107f9e4b2f306d7f
Yuxin <yuxinwu1234@gmail.com>
2023-10-30 22:44:42 +0800

    removed redundant prop

--- frontend/components/CodeEditor.tsx
+++ frontend/components/CodeEditor.tsx
@@ -1,6 +1,5 @@
 "use client";
 import {
-
 	Box,
 	Button,
 	Grid,
@@ -17,43 +16,41 @@ import { useEffect, useRef, useState } from "react";
 import { io } from "socket.io-client";
 import SaveHistoryButton from "./SaveHistoryButton";
 
-
 export default function CodeEditor({ socketRoom, matchedUser, colorMode }) {
-  const editorRef = useRef(null);
-  const [socket, setSocket] = useState(null);
-  const isIncomingCode = useRef(false);
-  const colorRef = useRef(null);
-  const [language, setLanguage] = useState("javascript");
-  const [code, setCode] = useState("//some comments");
-  const [theme, setTheme] = useState("light");
-  const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
-    editorRef.current = editor;
-  };
+	const editorRef = useRef(null);
+	const [socket, setSocket] = useState(null);
+	const isIncomingCode = useRef(false);
+	const colorRef = useRef(null);
+	const [language, setLanguage] = useState("javascript");
+	const [code, setCode] = useState("//some comments");
+	const [theme, setTheme] = useState("light");
+	const handleEditorDidMount = (editor: editor.IStandaloneCodeEditor) => {
+		editorRef.current = editor;
+	};
 
-  useEffect(() => {
-    handleThemeChange(colorMode);
-  }, [colorMode]);
-  const handleCodeChange = (
-    value = "",
-    event: editor.IModelContentChangedEvent
-  ) => {
-    if (isIncomingCode.current) {
-      isIncomingCode.current = false;
-      return;
-    }
-    console.log("sent");
-    setCode(editorRef.current.getModel().getValue());
-    socket?.emit("codeChange", event);
-  };
+	useEffect(() => {
+		handleThemeChange(colorMode);
+	}, [colorMode]);
+	const handleCodeChange = (
+		value = "",
+		event: editor.IModelContentChangedEvent
+	) => {
+		if (isIncomingCode.current) {
+			isIncomingCode.current = false;
+			return;
+		}
+		console.log("sent");
+		setCode(editorRef.current.getModel().getValue());
+		socket?.emit("codeChange", event);
+	};
 
-  const handleThemeChange = (e: string) => {
-    if ((window as any).monaco != undefined) {
-      (window as any).monaco.editor.setTheme(
-        e === "light" ? "light" : "vs-dark"
-      );
-    }
-  };
-
+	const handleThemeChange = (e: string) => {
+		if ((window as any).monaco != undefined) {
+			(window as any).monaco.editor.setTheme(
+				e === "light" ? "light" : "vs-dark"
+			);
+		}
+	};
 
 	const handleLanguageChange = (e) => {
 		(window as any).monaco.editor.setModelLanguage(
@@ -111,46 +108,52 @@ export default function CodeEditor({ socketRoom, matchedUser, colorMode }) {
 		}
 	};
 
+	return (
+		<Grid
+			templateColumns="repeat(4, 1fr)"
+			gap={5}
+			height="100%"
+			width="100%"
+		>
+			<GridItem>
+				<Menu>
+					<MenuButton as={Button} width="100%">
+						{language == "javascript"
+							? "Javascript"
+							: language == "python"
+							? "Python"
+							: language == "C++"
+							? "C++"
+							: "Java"}
+					</MenuButton>
+					<MenuList>
+						<MenuItem
+							onClick={handleLanguageChange}
+							value="javascript"
+						>
+							Javascript
+						</MenuItem>
+						<MenuItem onClick={handleLanguageChange} value="python">
+							Python
+						</MenuItem>
+						<MenuItem onClick={handleLanguageChange} value="C++">
+							C++
+						</MenuItem>
+						<MenuItem onClick={handleLanguageChange} value="Java">
+							Java
+						</MenuItem>
+					</MenuList>
+				</Menu>
+			</GridItem>
 
-  return (
-    <Grid templateColumns="repeat(4, 1fr)" gap={5} height="100%" width="100%">
-      <GridItem>
-        <Menu>
-          <MenuButton as={Button} width="100%">
-            {language == "javascript"
-              ? "Javascript"
-              : language == "python"
-              ? "Python"
-              : language == "C++"
-              ? "C++"
-              : "Java"}
-          </MenuButton>
-          <MenuList>
-            <MenuItem onClick={handleLanguageChange} value="javascript">
-              Javascript
-            </MenuItem>
-            <MenuItem onClick={handleLanguageChange} value="python">
-              Python
-            </MenuItem>
-            <MenuItem onClick={handleLanguageChange} value="C++">
-              C++
-            </MenuItem>
-            <MenuItem onClick={handleLanguageChange} value="Java">
-              Java
-            </MenuItem>
-          </MenuList>
-        </Menu>
-      </GridItem>
-
-      <GridItem>
-        <SaveHistoryButton
-          code={code}
-          theme={theme}
-          language={language}
-          difficulty={"Easy"}
-          matchedUser={matchedUser}
-        />
-        {/* <Button
+			<GridItem>
+				<SaveHistoryButton
+					code={code}
+					theme={theme}
+					language={language}
+					difficulty={"Easy"}
+				/>
+				{/* <Button
 
 						onClick={() =>
 							alert(editorRef.current.getModel().getValue())
@@ -160,22 +163,21 @@ export default function CodeEditor({ socketRoom, matchedUser, colorMode }) {
 					>
 						Save History
 					</Button> */}
-      </GridItem>
-      <GridItem>
-        <Button onClick={handleFormat} width="100%" colorScheme="blue">
-          Format Code
-        </Button>
-      </GridItem>
-      <GridItem colSpan={4}>
-        <Editor
-          onChange={handleCodeChange}
-          theme={colorMode == "light" ? "light" : "vs-dark"}
-          onMount={handleEditorDidMount}
-          defaultLanguage="javascript"
-          defaultValue="// some comment"
-        />
-      </GridItem>
-    </Grid>
-  );
-
+			</GridItem>
+			<GridItem>
+				<Button onClick={handleFormat} width="100%" colorScheme="blue">
+					Format Code
+				</Button>
+			</GridItem>
+			<GridItem colSpan={4}>
+				<Editor
+					onChange={handleCodeChange}
+					theme={colorMode == "light" ? "light" : "vs-dark"}
+					onMount={handleEditorDidMount}
+					defaultLanguage="javascript"
+					defaultValue="// some comment"
+				/>
+			</GridItem>
+		</Grid>
+	);
 }
