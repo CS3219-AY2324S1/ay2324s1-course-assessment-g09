52fa2ba45f83e998217c249b351fc98897143b0a
Wilson Ng <wilsonngja@gmail.com>
2023-11-05 20:39:32 +0800

    Improve Visuals in Custom Room

--- frontend/components/CreateCustomRoom.tsx
+++ frontend/components/CreateCustomRoom.tsx
@@ -1,6 +1,11 @@
 import {
   Box,
   Button,
+  Center,
+  Grid,
+  GridItem,
+  HStack,
+  Heading,
   Input,
   Menu,
   MenuButton,
@@ -69,52 +74,80 @@ export default function CreateCustomRoom() {
 
   return (
     <Box>
-      <Menu>
-        <MenuButton
+      <Heading
+        as="h5"
+        size="sm"
+        mb={3}
+        textDecoration="underline"
+        textColor={colorMode == "light" ? "green.500" : "green.300"}
+      >
+        Create Room
+      </Heading>
+      <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+        <GridItem>
+          <Menu>
+            <MenuButton
+              size={{ lg: "sm", xl: "sm", "2xl": "lg" }}
+              as={Button}
+              rightIcon={<ChevronDownIcon />}
+              colorScheme={complexityColor[difficulty]}
+            >
+              {difficulty == "Easy"
+                ? "Easy"
+                : difficulty == "Medium"
+                ? "Medium"
+                : difficulty == "Hard"
+                ? "Hard"
+                : "Difficulty"}
+            </MenuButton>
+            <MenuList>
+              <MenuItem
+                onClick={handleDifficultyChange}
+                value="Easy"
+                color={complexityTextColor["Easy"][colorMode]}
+                fontWeight="bold"
+              >
+                Easy
+              </MenuItem>
+              <MenuItem
+                onClick={handleDifficultyChange}
+                value="Medium"
+                color={complexityTextColor["Medium"][colorMode]}
+                fontWeight="bold"
+              >
+                Medium
+              </MenuItem>
+              <MenuItem
+                onClick={handleDifficultyChange}
+                value="Hard"
+                color={complexityTextColor["Hard"][colorMode]}
+                fontWeight="bold"
+              >
+                Hard
+              </MenuItem>
+            </MenuList>
+          </Menu>
+        </GridItem>
+        <GridItem colSpan={2}>
+          <Input
+            onChange={(e) => setRoomName(e.target.value)}
+            size={{ lg: "sm", xl: "sm", "2xl": "lg" }}
+            placeholder="Enter Room ID"
+          />
+        </GridItem>
+      </Grid>
+      <Center mt={4}>
+        <Button
+          onClick={handleCreateCustom}
           size={{ lg: "sm", xl: "sm", "2xl": "lg" }}
-          as={Button}
-          rightIcon={<ChevronDownIcon />}
-          colorScheme={complexityColor[difficulty]}
+          colorScheme="green"
         >
-          {difficulty == "Easy"
-            ? "Easy"
-            : difficulty == "Medium"
-            ? "Medium"
-            : difficulty == "Hard"
-            ? "Hard"
-            : "Difficulty"}
-        </MenuButton>
-        <MenuList>
-          <MenuItem
-            onClick={handleDifficultyChange}
-            value="Easy"
-            color={complexityTextColor["Easy"][colorMode]}
-            fontWeight="bold"
-          >
-            Easy
-          </MenuItem>
-          <MenuItem
-            onClick={handleDifficultyChange}
-            value="Medium"
-            color={complexityTextColor["Medium"][colorMode]}
-            fontWeight="bold"
-          >
-            Medium
-          </MenuItem>
-          <MenuItem
-            onClick={handleDifficultyChange}
-            value="Hard"
-            color={complexityTextColor["Hard"][colorMode]}
-            fontWeight="bold"
-          >
-            Hard
-          </MenuItem>
-        </MenuList>
-      </Menu>
-      <Input onChange={(e) => setRoomName(e.target.value)} />
+          Create Room
+        </Button>
+      </Center>
+
       {/* (roomExists &&
 			<Text>Please choose another name, the room already exist</Text>) */}
-      <Button onClick={handleCreateCustom}>Create and join room</Button>
     </Box>
   );
 }
--- frontend/components/JoinCustomRoom.tsx
+++ frontend/components/JoinCustomRoom.tsx
@@ -1,36 +1,73 @@
-import { Box, Input, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Input,
+  Button,
+  useColorMode,
+  Heading,
+  Grid,
+  GridItem,
+  Center,
+} from "@chakra-ui/react";
 import { useState } from "react";
 import socketManager from "./Sockets/CommunicationSocketManager";
 import matchSocketManager from "./Sockets/MatchSocketManager";
 import { useRouter } from "next/router";
 
 export default function JoinCustomRoom() {
-	const router = useRouter();
-	const [roomName, setRoomName] = useState("");
-	const handleJoinCustom = () => {
-		matchSocketManager.emitEvent("match", {
-			condition: roomName,
-			difficulty: "",
-			user: JSON.parse(sessionStorage.getItem("login")).email,
-			videoSocket: socketManager.getSocketId(),
-		});
-		matchSocketManager.subscribeToEvent("matched", (data) => {
-			const matchedUser = data.user;
-			const matchedVideoSocket = data.videoSocket;
-			const room = data.room;
-			const difficulty = data.difficulty;
-			console.log("matched", matchedUser, matchedVideoSocket, room);
-			matchSocketManager.setMatchedDifficulty(difficulty);
-			socketManager.setMatchedSocketId(matchedVideoSocket);
-			matchSocketManager.setMatchedUser(matchedUser);
-			matchSocketManager.setMatchedRoom(room);
-			router.push("/collaboration");
-		});
-	};
-	return (
-		<Box>
-			<Input onChange={(e) => setRoomName(e.target.value)} />
-			<Button onClick={handleJoinCustom}>Join room</Button>
-		</Box>
-	);
+  const { colorMode } = useColorMode();
+
+  const router = useRouter();
+  const [roomName, setRoomName] = useState("");
+  const handleJoinCustom = () => {
+    matchSocketManager.emitEvent("match", {
+      condition: roomName,
+      difficulty: "",
+      user: JSON.parse(sessionStorage.getItem("login")).email,
+      videoSocket: socketManager.getSocketId(),
+    });
+    matchSocketManager.subscribeToEvent("matched", (data) => {
+      const matchedUser = data.user;
+      const matchedVideoSocket = data.videoSocket;
+      const room = data.room;
+      const difficulty = data.difficulty;
+      console.log("matched", matchedUser, matchedVideoSocket, room);
+      matchSocketManager.setMatchedDifficulty(difficulty);
+      socketManager.setMatchedSocketId(matchedVideoSocket);
+      matchSocketManager.setMatchedUser(matchedUser);
+      matchSocketManager.setMatchedRoom(room);
+      router.push("/collaboration");
+    });
+  };
+  return (
+    <Box>
+      <Heading
+        as="h5"
+        size="sm"
+        mb={3}
+        textDecoration="underline"
+        textColor={colorMode == "light" ? "purple.500" : "purple.300"}
+      >
+        Join Room
+      </Heading>
+
+      <Grid templateColumns="repeat(3, 1fr)" gap={4}>
+        <GridItem colSpan={3}>
+          <Input
+            onChange={(e) => setRoomName(e.target.value)}
+            size={{ lg: "sm", xl: "sm", "2xl": "lg" }}
+            placeholder="Enter Room ID"
+          />
+        </GridItem>
+      </Grid>
+      <Center mt={4}>
+        <Button
+          onClick={handleJoinCustom}
+          colorScheme="purple"
+          size={{ lg: "sm", xl: "sm", "2xl": "lg" }}
+        >
+          Join room
+        </Button>
+      </Center>
+    </Box>
+  );
 }
